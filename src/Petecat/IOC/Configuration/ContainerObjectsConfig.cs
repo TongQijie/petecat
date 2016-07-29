209dfbb51ec51f694eab1f476a2e62bb2e24f960
Jerry Tong <jerrytong_90@hotmail.com>
2016-07-29 17:19:49 +0800

    add container objects config

---
+++ src/Petecat/IOC/Configuration/ContainerObjectsConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Petecat.IOC.Configuration
+{
+    [XmlRoot("objects")]
+    public class ContainerObjectsConfig
+    {
+        [XmlElement("assembly")]
+        public ContainerAssemblyConfig[] Assemblies { get; set; }
+
+        [XmlElement("import")]
+        public ContainerImporterConfig[] Importers { get; set; }
+
+        [XmlElement("object")]
+        public ContainerObjectConfig[] Objects { get; set; }
+    }
+}
