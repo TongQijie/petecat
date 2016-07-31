9cd55c3d7aca27a59fbfb007d8a540724fe1d8ee
Jerry <jerrytong_90@hotmail.com>
2016-07-31 14:51:39 +0800

    add ftp client

---
+++ src/Petecat/Network/Ftp/FtpClientRequest.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+
+namespace Petecat.Network.Ftp
+{
+    public class FtpClientRequest
+    {
+        private FtpClientRequest(FtpVerb ftpVerb)
+        {
+            FtpVerb = ftpVerb;
+        }
+
+        public FtpClientRequest(FtpVerb ftpVerb, string uri)
+            : this(ftpVerb)
+        {
+            Request = WebRequest.Create(uri) as FtpWebRequest;
+
+            if (FtpVerb == FtpVerb.DownloadFile)
+            {
+                Request.Method = "RETR";
+            }
+            else if (FtpVerb == FtpVerb.UploadFile)
+            {
+                Request.Method = "STOR";
+            }
+            else if (FtpVerb == FtpVerb.ListDirectory)
+            {
+                Request.Method = "NLST";
+            }
+        }
+
+        public FtpWebRequest Request { get; private set; }
+
+        public FtpVerb FtpVerb { get; private set; }
+
+        public FtpClientResponse GetResponse()
+        {
+            try
+            {
+                return new FtpClientResponse(Request.GetResponse() as FtpWebResponse);
+            }
+            catch (WebException e)
+            {
+                return new FtpClientResponse(e.Response as FtpWebResponse);
+            }
+        }
+    }
+}
---
+++ test/Petecat.Test/Network/Ftp/FtpClientTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Petecat.Network.Ftp;
+using System.Net;
+using System.Text;
+
+namespace Petecat.Test.Network.Ftp
+{
+    [TestClass]
+    public class FtpClientTest
+    {
+        [TestMethod]
+        public void DownloadFile()
+        {
+            var request = new FtpClientRequest(FtpVerb.DownloadFile, "ftp://192.168.0.105/hey.txt");
+
+            using (var response = request.GetResponse())
+            {
+                var data = response.GetString(Encoding.Default);
+            }
+        }
+
+        [TestMethod]
+        public void ListDirectory()
+        {
+            var request = new FtpClientRequest(FtpVerb.ListDirectory, "ftp://192.168.0.105");
+
+            using (var response = request.GetResponse())
+            {
+                var data = response.GetString(Encoding.Default);
+            }
+        }
+
+        [TestMethod]
+        public void UploadFile()
+        {
+            var request = new FtpClientRequest(FtpVerb.UploadFile, "ftp://192.168.0.105/uploadedFile1.txt");
+            using (var stream = request.Request.GetRequestStream())
+            {
+                var data = Encoding.UTF8.GetBytes("uploaded file.dddd");
+                stream.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
