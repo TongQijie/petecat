a93cdbc1d33478f8aef2d7736b88f5eed6df0826
jt69 <jt69@newegg.com>
2016-09-13 17:04:26 +0800

    add serviceproxy and servicetest

---
+++ app/blogservices/articleservice.serviceproxy/ArticleServiceProxy.cs
@@ -0,0 +1,23 @@
+﻿using ArticleService.ServiceModel;
+using ArticleService.ServiceModel.Infrastructure;
+using Petecat.Service.Client;
+namespace ArticleService.ServiceProxy
+{
+    public static class ArticleServiceProxy
+    {
+        public static ServiceResponse<ArticleInfo[]> GetArticlesByPage(ServiceRequest request)
+        {
+            return new ServiceClientBase("get-articles-by-page").Call<ServiceResponse<ArticleInfo[]>>(request);
+        }
+
+        public static ServiceResponse<ArticleInfo> GetArticleById(ServiceRequest request)
+        {
+            return new ServiceClientBase("get-article-by-id").Call<ServiceResponse<ArticleInfo>>(request);
+        }
+
+        public static ServiceResponse EditArticleById(ServiceRequest<ArticleInfo> request)
+        {
+            return new ServiceClientBase("edit-article-by-id").Call<ServiceResponse>(request);
+        }
+    }
+}
---
+++ app/blogservices/articleservice.servicetest/Program.cs
@@ -0,0 +1,35 @@
+﻿using ArticleService.ServiceModel;
+using ArticleService.ServiceModel.Infrastructure;
+using ArticleService.ServiceProxy;
+using System;
+namespace articleservice.servicetest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var articles = ArticleServiceProxy.GetArticlesByPage(new ServiceRequest()
+            {
+                Paging = new Paging(1, 10),
+            });
+            var article = ArticleServiceProxy.GetArticleById(new ServiceRequest()
+            {
+                KeyValues = new KeyValuePair[] 
+                { 
+                    new KeyValuePair("ArticleId", "hello-world"),
+                },
+            });
+            ArticleServiceProxy.EditArticleById(new ServiceRequest<ArticleInfo>()
+            {
+                Body = new ArticleInfo()
+                {
+                    Id = Environment.TickCount.ToString(),
+                    CreationDate = DateTime.Now,
+                    Title = "hello, World!",
+                    Abstract = "hey, man",
+                    Content = "this is a note about butterfly.",
+                },
+            });
+        }
+    }
+}
