47fd75cc86814b14a04bed28cefb152a020ecd64
jt69 <jt69@newegg.com>
2016-10-12 18:08:46 +0800

    add processobject

---
+++ src/Threading/Process/ProcessObject.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+namespace Petecat.Threading.Process
+{
+    public class ProcessObject
+    {
+        public ProcessObject(string executable)
+        {
+            Executable = executable;
+        }
+
+        public string Executable { get; private set; }
+
+        public string Arguments { get; private set; }
+
+        public ProcessObject Add(string argument)
+        {
+            Arguments += " " + argument;
+            return this;
+        }
+
+        public string ReadString(string workingDirectory = null)
+        {
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = Executable;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.Arguments = Arguments;
+            process.StartInfo.WorkingDirectory = workingDirectory;
+            process.Start();
+
+            using (var reader = process.StandardOutput)
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public StreamReader ReadStream(string workingDirectory = null)
+        {
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = Executable;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.Arguments = Arguments;
+            process.StartInfo.WorkingDirectory = workingDirectory;
+            process.Start();
+
+            return process.StandardOutput;
+        }
+    }
+}
---
+++ test/petecat.test/Threading/Process/ProcessObjectTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Petecat.Threading.Process;
+using System;
+using System.Collections.Generic;
+
+namespace Petecat.Test.Threading.Process
+{
+    [TestClass]
+    public class ProcessObjectTest
+    {
+        [TestMethod]
+        public void ExecuteTest()
+        {
+            var changeFiles = new List<ChangeFile>();
+
+            using (var reader = new ProcessObject("git")
+                .Add("diff").Add("--name-status").Add("master..project_13400")
+                .ReadStream(@"d:\git\itemservice"))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var fields = line.Split('\t');
+                    if (fields.Length == 2)
+                    {
+                        changeFiles.Add(new ChangeFile() { Action = fields[0].Trim(), Path = fields[1].Trim() });
+                    }
+                }
+            }
+
+            var commits = new List<Commit>();
+
+            using (var reader = new ProcessObject("git")
+                .Add("log").Add("master..")
+                .ReadStream(@"d:\git\itemservice"))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("commit", StringComparison.OrdinalIgnoreCase))
+                    {
+                        commits.Add(new Commit() { Id = line.Remove(0, 6).Trim() });
+                    }
+                }
+            }
+
+            //var initialVersion = new ProcessObject("git")
+            //    .Add("show").Add("fcc8ae29481f7abf96a5d2cb8ae34ab7f5d58d88:Src/Item.Business/Implement/ItemBusiness.cs")
+            //    .ReadString(@"d:\git\itemservice");
+
+            //var latestVersion = new ProcessObject("git")
+            //    .Add("show").Add("5464bd66a341ddb5878fd7f2e36672b84c369f90:Src/Item.Business/Implement/ItemBusiness.cs")
+            //    .ReadString(@"d:\git\itemservice");
+
+        }
+
+        class ChangeFile
+        {
+            public string Action { get; set; }
+
+            public string Path { get; set; }
+        }
+
+        class Commit
+        {
+            public string Id { get; set; }
+        }
+    }
+}
