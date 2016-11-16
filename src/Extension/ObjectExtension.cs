437a255b55da9a91adfcf8cd7f4cf5cccbaf5179
Jerry <jerrytong_90@hotmail.com>
2016-11-16 20:30:40 +0800

    add objectextension

---
+++ src/Extension/ObjectExtension.cs
@@ -0,0 +1,16 @@
+﻿using Petecat.Formatter;
+using Petecat.DependencyInjection;
+
+using System.Text;
+
+namespace Petecat.Extension
+{
+    public static class ObjectExtension
+    {
+        public static T Copy<T>(this T obj)
+        {
+            var formatter = DependencyInjector.GetObject<IJsonFormatter>();
+            return formatter.ReadObject<T>(formatter.WriteString(obj, Encoding.UTF8), Encoding.UTF8);
+        }
+    }
+}
